722dcebb9e77668111efb4c09e3d61b98858c786
dddd <lanou3g@lanou3gdeMac-mini-000000.local>
2016-04-15 14:35:42 +0800

    no message

---
+++ DotaRampage/DotaRampage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DotaRampage
+//
+//  Created by lanou3g on 16/4/15.
+//  Copyright © 2016年 张少华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DotaRampage/DotaRampage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DotaRampage
+//
+//  Created by lanou3g on 16/4/15.
+//  Copyright © 2016年 张少华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
